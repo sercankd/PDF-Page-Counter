a7c4ad8b5020935f5a1bc4b3e4a0209d2cd18921
sercankd <sercankd@DESKTOP-GJQD4F3>
2017-02-17 23:43:44 +0300

    Add project files.

---
+++ PDF Page Counter/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace PDF_Page_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.headerFile = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.headerFilesize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.headerPagescount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.headerFilepath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.headerFile,
+            this.headerFilesize,
+            this.headerPagescount,
+            this.headerFilepath});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(805, 425);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // headerFile
+            // 
+            this.headerFile.Text = "File";
+            this.headerFile.Width = 200;
+            // 
+            // headerFilesize
+            // 
+            this.headerFilesize.Text = "Filesize";
+            this.headerFilesize.Width = 100;
+            // 
+            // headerPagescount
+            // 
+            this.headerPagescount.Text = "Pages Count";
+            this.headerPagescount.Width = 100;
+            // 
+            // headerFilepath
+            // 
+            this.headerFilepath.Text = "File Path";
+            this.headerFilepath.Width = 400;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(512, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Drag & Drop Files or Folders into list below";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BorderStyle = System.Windows.Forms.Border3DStyle.Etched;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 19);
+            this.toolStripStatusLabel1.Text = "Items:";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(14, 19);
+            this.toolStripStatusLabel3.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.toolStripStatusLabel2.BorderStyle = System.Windows.Forms.Border3DStyle.Etched;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(105, 19);
+            this.toolStripStatusLabel2.Text = "Total Page Count:";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.BorderStyle = System.Windows.Forms.Border3DStyle.Etched;
+            this.toolStripStatusLabel4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(14, 19);
+            this.toolStripStatusLabel4.Text = "0";
+            // 
+            // toolStripStatusLabel6
+            // 
+            this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+            this.toolStripStatusLabel6.Size = new System.Drawing.Size(517, 19);
+            this.toolStripStatusLabel6.Spring = true;
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripStatusLabel5.IsLink = true;
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(72, 19);
+            this.toolStripStatusLabel5.Text = "Github Page";
+            this.toolStripStatusLabel5.Click += new System.EventHandler(this.toolStripStatusLabel5_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel6,
+            this.toolStripStatusLabel5});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 455);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(805, 24);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 479);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(821, 518);
+            this.MinimumSize = new System.Drawing.Size(570, 400);
+            this.Name = "Form1";
+            this.Text = "PDF Page Counter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader headerFile;
+        private System.Windows.Forms.ColumnHeader headerFilesize;
+        private System.Windows.Forms.ColumnHeader headerPagescount;
+        private System.Windows.Forms.ColumnHeader headerFilepath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ PDF Page Counter/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using System.Drawing;
+using System.Linq;
+
+namespace PDF_Page_Counter
+{
+    public partial class Form1 : Form
+    {
+        static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        public Form1()
+        {
+            Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.NonClientAreaEnabled;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.AllowDrop = true;
+            listView1.DragDrop += new DragEventHandler(listView1_DragDrop);
+            listView1.DragEnter += new DragEventHandler(listView1_DragEnter);
+        }
+        void listView1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        void listView1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] handles = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (string s in handles)
+            {
+                if (File.Exists(s))
+                {
+                    if (string.Compare(Path.GetExtension(s), ".pdf", true) == 0)
+                    {
+                        AddFileToListview(s);
+                    }
+                }
+                else if (Directory.Exists(s))
+                {
+                    DirectoryInfo di = new DirectoryInfo(s);
+                    FileInfo[] files = di.GetFiles("*.pdf", SearchOption.AllDirectories);
+                    foreach (FileInfo file in files)
+                        AddFileToListview(file.FullName);
+                }
+            }
+
+        }
+        private void AddFileToListview(string fullFilePath)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            if (!File.Exists(fullFilePath))
+                return;
+            string fileName = Path.GetFileName(fullFilePath);
+            string dirName = Path.GetDirectoryName(fullFilePath);
+            if (dirName.EndsWith(Convert.ToString(Path.DirectorySeparatorChar)))
+                dirName = dirName.Substring(0, dirName.Length - 1);
+            ListViewItem itm = listView1.Items.Add(fileName);
+            FileInfo fFile = new FileInfo(fileName);
+            long length = new System.IO.FileInfo(fullFilePath).Length;
+            itm.SubItems.Add(SizeSuffix(length)); //size column
+            PdfReader pdfReader = new PdfReader(fullFilePath);
+            int numberOfPages = pdfReader.NumberOfPages;
+            itm.SubItems.Add(numberOfPages.ToString()); //pages count
+            itm.SubItems.Add(dirName); //path
+
+            //calculate items count with linq
+            int countItems = listView1.Items.Cast<ListViewItem>().Count();
+            toolStripStatusLabel3.Text = countItems.ToString();
+
+            //calculate items count with linq
+            int countTotalPages = listView1.Items.Cast<ListViewItem>().Sum(item => int.Parse(item.SubItems[2].Text));
+            toolStripStatusLabel4.Text = countTotalPages.ToString();
+            Cursor.Current = Cursors.Default;
+
+        }
+        static string SizeSuffix(Int64 value, int decimalPlaces = 1)
+        {
+            if (value < 0) { return "-" + SizeSuffix(-value); }
+            if (value == 0) { return "0.0 bytes"; }
+
+            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+            int mag = (int)Math.Log(value, 1024);
+
+            // 1L << (mag * 10) == 2 ^ (10 * mag) 
+            // [i.e. the number of bytes in the unit corresponding to mag]
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+            // make adjustment when the value is large enough that
+            // it would round up to 1000 or more
+            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
+            {
+                mag += 1;
+                adjustedSize /= 1024;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                SizeSuffixes[mag]);
+        }
+
+        private void toolStripStatusLabel5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/sercankd/PDF-Page-Counter/");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = listView1.Items.Cast<ListViewItem>().Count();
+            toolStripStatusLabel4.Text = count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            toolStripStatusLabel3.Text = "0";
+            toolStripStatusLabel4.Text = "0";
+
+        }
+    }
+}
